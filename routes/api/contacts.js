8437744ad245996b10ffa9f299b279e721667c27
AndrewGumenchuk <64306832+AndrewGumenchuk@users.noreply.github.com>
2021-03-10 17:56:58 +0200

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const contactsController = require('../../controller');
 const { validateContact } = require('../../service/validation');
 
-router
-  .get('/', contactsController.listContacts)
-  .post('/', validateContact, contactsController.addContact);
 
-router
-  .get('/:contactId', contactsController.getContactById)
-  .delete('/:contactId', contactsController.removeContact)
-  .patch('/:contactId', validateContact, contactsController.updateContact);
+  router.get('/', contactsController.listContacts)
+  router.post('/', validateContact, contactsController.addContact);
+
+
+  router.get('/:contactId', contactsController.getContactById)
+  router.delete('/:contactId', contactsController.removeContact)
+  router.patch('/:contactId', validateContact, contactsController.updateContact);
 
 module.exports = router;
