f1dbf39e5ccb39b812cb70294be4a7dba8f5adf7
AndrewGumenchuk <64306832+AndrewGumenchuk@users.noreply.github.com>
2021-03-13 20:39:53 +0200

    routes/contacts.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 
-const contactsRouter = require('./controller/contacts');
+const contactsRouter = require('./controller');
 
 const app = express();
 
